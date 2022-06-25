8c01ccaa1552ba2b4a0e748b8685b43d936f335c
displaycoffee <adria.m.murphy@gmail.com>
2022-06-25 10:48:53 -0600

    fix dollar amount and add campaign total

--- assets/src/js/components/Campaign.jsx
+++ assets/src/js/components/Campaign.jsx
@@ -6,17 +6,20 @@ const Campaign = (props) => {
 	const campaignUrl = `${campaign.url}/${campaign.slug}`;
 	const campaignSize = utils.size(campaign);
 
+	console.log(campaign)
+
 	return (
 		<section className="detail detail-campaign">	
 			<h3 className="detail-title">About this campaign</h3>
 			
 			<div className={`detail-row${campaignSize ? ' detail-row-loaded' : ''} flex-wrap`}>
-				<Skeleton columns={1} paragraphs={3} />
+				<Skeleton columns={1} paragraphs={4} />
 
 				{campaignSize ? (
 					<div className="detail-column">
 						<div className="detail-column-inner">
-							<p><strong>Total Raised:</strong> ${campaign.totalAmountRaised}</p>
+							<p><strong>Total Raised:</strong> ${campaign.totalAmountRaised.toFixed(2)}</p>
+							<p><strong>Current Goal:</strong> ${campaign.fundraiserGoalAmount.toFixed(2)}</p>
 							<p><strong>About:</strong> {campaign.description}</p>
 							<p><a href={campaignUrl} target="_blank">{campaignUrl.replace('https://', '')}</a></p>
 						</div>
