a1e39540e3629689f98882c49644c7a571e2fd40
displaycoffee <adria.m.murphy@gmail.com>
2026-04-12 14:57:14 -0600

    quick change

--- src/components/navigation/scripts/navigation-utils.ts
+++ src/components/navigation/scripts/navigation-utils.ts
@@ -55,22 +55,20 @@ export const navigationUtils = {
 		},
 	},
 	routes: {
-		build: {
-			config: (nav: NavigationType) => {
-				// Build route config
-				const config = {
-					id: nav.id,
-					element: nav.element,
-					path: nav.url,
-				} as NavigationRoutesType;
+		build: (nav: NavigationType) => {
+			// Build route config
+			const config = {
+				id: nav.id,
+				element: nav.element,
+				path: nav.url,
+			} as NavigationRoutesType;
 
-				// Add props if there are any
-				if (nav?.props) {
-					config.props = nav.props;
-				}
+			// Add props if there are any
+			if (nav?.props) {
+				config.props = nav.props;
+			}
 
-				return config;
-			},
+			return config;
 		},
 	},
 	sort: (list: NavigationType[]) => {
