2cb59f64bc0c2d3b740f0cbee009b60324257432
displaycoffee <adria.m.murphy@gmail.com>
2026-05-25 08:39:28 -0600

    change a few things

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -41,9 +41,9 @@ export const ContextProvider = ({ children }: ContextProps) => {
 	// Create state for app
 	const contentConfig: ContentType = {
 		totals: {
-			amountRaised: 0,
-			goal: 0,
-			totalRaised: 0,
+			amountRaised: 3011.68,
+			goal: 3000.0,
+			totalRaised: 53881.81,
 		},
 		campaign: {
 			fetched: false,
