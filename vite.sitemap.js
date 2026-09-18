00819ba5fde37cd7c5cd6e68fdb60af838259df1
Adria <displaycoffee@users.noreply.github.com>
2026-09-18 09:41:20 -0600

    Update vite.sitemap.js

--- vite.sitemap.js
+++ vite.sitemap.js
@@ -1,4 +1,7 @@
+/* Scripts */
 import packageJSON from './package.json' with { type: 'json' };
+
+/* Variables */
 const hostname = packageJSON.homepage || 'https://localhost:3000';
 const location = new URL(hostname);
 
@@ -8,8 +11,7 @@ let sitemap = {
 	exclude: ['/assets', '/assets/css', '/assets/fonts', '/assets/images', '/assets/images/theme', '/assets/js'],
 	dynamicRoutes: ['/participant-guide', '/dashboard', '/donations'],
 };
-if (location?.pathname && location.pathname != '/') {
-	sitemap.basePath = location.pathname;
-}
+
+if (location?.pathname && location.pathname != '/') sitemap.basePath = location.pathname;
 
 export const sitemapConfig = sitemap;
